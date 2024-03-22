7b05c3f133dc6ed7efc7c3bbb2058dd9c1b757ad
arturkhrabrov <khrabrovart@gmail.com>
2024-03-22 13:37:46 +0400

    Initial commit

---
+++ src/SpotiMate/SpotiMate/Cli/CliOptions.cs
@@ -0,0 +1,15 @@
+using CommandLine;
+
+namespace SpotiMate.Cli;
+
+public class CliOptions
+{
+    [Option("client-id", Required = true, HelpText = "Spotify client id.")]
+    public string ClientId { get; set; }
+    
+    [Option("client-secret", Required = true, HelpText = "Spotify client secret.")]
+    public string ClientSecret { get; set; }
+    
+    [Option("refresh-token", Required = true, HelpText = "Spotify refresh token.")]
+    public string RefreshToken { get; set; }
+}
---
+++ src/SpotiMate/SpotiMate/Program.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using Flurl;
+using Flurl.Http;
+using SpotiMate.Cli;
+
+namespace SpotiMate;
+
+public class Program
+{
+    private const string SpotifyAccounts = "https://accounts.spotify.com";
+
+    public static async Task Main(string[] args)
+    {
+        await Parser.Default.ParseArguments<CliOptions>(args).WithParsedAsync(Run);
+    }
+    
+    private static async Task Run(CliOptions options)
+    {
+        var token = await GetAccessToken(options.ClientId, options.ClientSecret, options.RefreshToken);
+    }
+    
+    private static async Task<SpotifyTokenResponse> GetAccessToken(
+        string clientId,
+        string clientSecret, 
+        string refreshToken)
+    {
+        return await SpotifyAccounts
+            .AppendPathSegment("api/token")
+            .WithBasicAuth(clientId, clientSecret)
+            .PostUrlEncodedAsync(new
+            {
+                refresh_token = refreshToken,
+                grant_type = "refresh_token"
+            })
+            .ReceiveJson<SpotifyTokenResponse>();
+    }
+}
